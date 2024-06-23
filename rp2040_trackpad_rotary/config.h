913285365f42daa89c0bf53e26dcffd924d22ff8
bushikov <bushikov@users.noreply.github.com>
2024-06-24 03:21:12 +0900

    Seesd Xiao RP2040でAzoteq TPS43を使ってポインティングデバイスを試した

---
+++ rp2040_trackpad_rotary/config.h
@@ -0,0 +1,10 @@
+#pragma once
+
+#define ENCODERS_PAD_A { GP2 }
+#define ENCODERS_PAD_B { GP4 }
+
+#define I2C_DRIVER I2CD1
+#define I2C1_SDA_PIN GP2
+#define I2C1_SCL_PIN GP3
+
+#define AZOTEQ_IQS5XX_TPS43
---
+++ rp2040_trackpad_rotary/readme.md
@@ -0,0 +1,7 @@
+# rp2040_trackpad_rotary
+
+Azoteq TPS43を使ったポインティングデバイス
+
+rotaryは一旦外してる
+
+![試作](./images/image001.jpg)
